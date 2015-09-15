de2bb3b351ed0524fb0ef94d9ea84018e2966c69
Zheng Rong <zheng.rong.14@gmail.com>
2015-09-15 01:28:58 -0400

    add launch file

--- mv_camera/src/sync_mono_imu.cpp
+++ mv_camera/src/sync_mono_imu.cpp
@@ -9,7 +9,7 @@
 
 using namespace std;
 
-#define IMAGE_FRAME_TIME_TOLERANCE 2.5e-2     //25ms
+#define IMAGE_FRAME_TIME_TOLERANCE 3.5e-2     //25ms
 
 // buffer used to store the msgs
 std::vector<sensor_msgs::CameraInfo> mono_info_buff(0);
@@ -55,11 +55,9 @@ void mono_info_callback(const sensor_msgs::CameraInfoConstPtr& info)
     mono_info_buff.push_back(*info);
 }
 
-
 //=======================================================================================
 // correct the mono image time stamp with px4-gpio time domain
 //=======================================================================================
-
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "sync_mono_imu");
@@ -67,26 +65,24 @@ int main(int argc, char **argv)
 
     std::string ns = nh.getNamespace();
 
-    std::string sub_time_topic_name = ros::names::append(ns, "gpio_time");
-    std::string sub_mono_image_topic_name = ros::names::append(ns, "image_raw");
-    std::string sub_mono_info_topic_name = ros::names::append(ns, "camera_info");
-    // std::string sub_imu_topic_name = "/mavlink/imu";
+    std::string sub_time_topic_name = "/icc/gpio_time";
+    std::string sub_mono_image_topic_name = "/mono/image_rect";
+    //std::string sub_mono_info_topic_name = "/mono/camera_info";
     //std::string sub_imu_topic_name = ros::names::append(ns, "imu/imu");
 
-
-    std::string pub_time_topic_name = ros::names::append(ns, "sync/gpio_time");
-    std::string pub_mono_image_topic_name = ros::names::append(ns, "sync/image_raw");
-    std::string pub_mono_info_topic_name = ros::names::append(ns, "sync/camera_info");
+    std::string pub_time_topic_name = "/sync/gpio_time";
+    std::string pub_mono_image_topic_name = "/sync/image_rect";
+    //std::string pub_mono_info_topic_name = ros::names::append(ns, "sync/camera_info");
     // std::string pub_imu_topic_name = ros::names::append(ns, "sync/imu");
 
     image_transport::ImageTransport it(nh);
     
     ros::Subscriber sub_time = 
-            nh.subscribe(sub_time_topic_name, 1, gpio_time_callback);
+            nh.subscribe(sub_time_topic_name, 5, gpio_time_callback);
     image_transport::Subscriber sub_mono_image = 
-            it.subscribe(sub_mono_image_topic_name, 1, mono_image_callback);
-    ros::Subscriber sub_mono_info = 
-            nh.subscribe(sub_mono_info_topic_name, 1, mono_info_callback);
+            it.subscribe(sub_mono_image_topic_name, 5, mono_image_callback);
+    // ros::Subscriber sub_mono_info = 
+    //         nh.subscribe(sub_mono_info_topic_name, 1, mono_info_callback);
     // ros::Subscriber sub_imu = 
     //         nh.subscribe(sub_imu_topic_name, 1, imu_callback);
 
@@ -96,44 +92,44 @@ int main(int argc, char **argv)
             nh. advertise<quadrotor_msgs::GPIOTime>(pub_time_topic_name, 5);
     ros::Publisher pub_mono_image = 
             nh.advertise<sensor_msgs::Image>(pub_mono_image_topic_name, 5);
-    ros::Publisher pub_mono_info = 
-            nh.advertise<sensor_msgs::CameraInfo>(pub_mono_info_topic_name, 5);
+    // ros::Publisher pub_mono_info = 
+    //         nh.advertise<sensor_msgs::CameraInfo>(pub_mono_info_topic_name, 5);
     // ros::Publisher pub_imu = nh.advertise<sensor_msgs::Imu>(pub_imu_topic_name, 5);
 
-    ros::Rate rate(100); 
+    ros::Rate rate(1000); 
 
     while(ros::ok())
     {
         
         while(  (mono_image_buff.size() > 0) &&  
-                (time_buff.size() > 0) && 
-                (mono_info_buff.size() > 0))
+                (time_buff.size() > 0)) // && 
+                // (mono_info_buff.size() > 0))
         {
             compare_vector.at(0) = time_buff[0].header.stamp.toSec();
             compare_vector.at(1) = mono_image_buff[0].header.stamp.toSec();
-            compare_vector.at(2) = mono_info_buff[0].header.stamp.toSec();
+            // compare_vector.at(2) = mono_info_buff[0].header.stamp.toSec();
 
             compare_iter = compare_vector.begin();
 
-            max_iter = max_element(compare_iter, compare_iter+3);
-            min_iter = min_element(compare_iter, compare_iter+3);
+            max_iter = max_element(compare_iter, compare_iter+2);
+            min_iter = min_element(compare_iter, compare_iter+2);
 
             // three time stamps match
             if(*max_iter - *min_iter < IMAGE_FRAME_TIME_TOLERANCE)
             {
                 //change time of image and info
                 mono_image_buff.at(0).header.stamp = time_buff.at(0).ref1.stamp;
-                mono_info_buff.at(0).header.stamp = time_buff.at(0).ref1.stamp;
+                // mono_info_buff.at(0).header.stamp = time_buff.at(0).ref1.stamp;
 
                 // publish the msgs
                 pub_time.publish(time_buff.at(0));
                 pub_mono_image.publish(mono_image_buff.at(0));
-                pub_mono_info.publish(mono_info_buff.at(0));
+                // pub_mono_info.publish(mono_info_buff.at(0));
 
                 // delete the published msgs from the vector
                 time_buff.erase(time_buff.begin());
                 mono_image_buff.erase(mono_image_buff.begin());
-                mono_info_buff.erase(mono_info_buff.begin());
+                // mono_info_buff.erase(mono_info_buff.begin());
             }
             // there are at least one stamp-time that is too old 
             // which may be caused by frame/message lost
@@ -150,19 +146,17 @@ int main(int argc, char **argv)
                     mono_image_buff.erase(mono_image_buff.begin());
                     ROS_WARN("Mono-image message is too old!");
                 }
-                else if(min_iter == compare_iter+2)
-                {
-                    mono_info_buff.erase(mono_info_buff.begin());
-                    ROS_WARN("Mono-info message is too old!");
-                }
+                // else if(min_iter == compare_iter+2)
+                // {
+                //     mono_info_buff.erase(mono_info_buff.begin());
+                //     ROS_WARN("Mono-info message is too old!");
+                // }
             }
 
         }
-
         ros::spinOnce();
         rate.sleep();
     }
-
     return 0;
 }
 
